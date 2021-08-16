ba6d748ed685e6f094e10bfaa64c5c6dc5d4c46c
Rhaegar2012 <48417377+Rhaegar2012@users.noreply.github.com>
2021-08-16 00:19:45 -0700

    08162021-ContactForm Update

--- FinalExam/Contact/views.py
+++ FinalExam/Contact/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
+from os import name
+from django.http.response import HttpResponse
+from django.shortcuts import render, redirect
 from .models import Contact 
+from .forms import UserRegisterForm
+from django.core.mail import send_mail
+
 # Create your views here.
 def contactView(request):
     contacts=Contact.objects.all()
     context ={
         "contacts":contacts
     }
-    return render(request,"Contact/Contact.html",context)
+    return render(request,"Contact/register.html",context)
+
+def register(request):
+    if request.method == 'POST':
+      form = UserRegisterForm(request.POST)
+      if form.is_valid(): 
+        form.save()
+        username = form.cleaned_data.get('username')
+        send_mail(subject='New User', message='You have a new User on your website', from_email='xiaomei.mandy.li@gmail.com', recipient_list=['tanjajt@gmail.com'])
+        return redirect('register')
+    else:
+       form = UserRegisterForm()
+    return render(request, 'Contact/register.html', {'form': form})
